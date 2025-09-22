5e540df07e1bade6c881ad15ab4435550a93fd5f
Vindya Kodithuwakku <164727679+vindyakodithuwakku02@users.noreply.github.com>
2025-09-22 09:11:30 +0530

    update coupon routes

---
+++ app/api/admin/coupon/route.js
@@ -0,0 +1,71 @@
+import prisma from "@/lib/prisma";
+import authAdmin from "@/middlewares/authAdmin";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+
+//add new coupon
+export async function POST(request){
+    try {
+        const {userId} = getAuth(request)
+        const isAdmin = await authAdmin(userId)
+
+        if(!isAdmin){
+            return NextResponse.json({ error: "not authorized"} , { status: 401 })
+        }
+
+        const {coupon} = await request.json()
+        coupon.code = coupon.code.toUpperCase()
+
+        await prisma.coupon.create({data: coupon})
+
+        return NextResponse.json({message: "coupon added successfully"})
+
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json({ error: error.code || error.message} , {status: 400})        
+    }
+}
+
+//Delete coupon /api/coupon?id=couponId
+export async function DELETE(request){
+    try {
+        const {userId} = getAuth(request)
+        const isAdmin = await authAdmin(userId)
+
+        if(!isAdmin){
+            return NextResponse.json({ error: "not authorized"} , { status: 401 })
+        }
+
+        const { searchParams} = request.nextUrl;
+        const code = searchParams.get('code') 
+
+        await prisma.coupon.delete({where: {code}})
+
+        return NextResponse.json({message: "coupon deleted successfully"})
+
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json({ error: error.code || error.message} , {status: 400})        
+    }
+}
+
+//Get all coupons
+export async function GET(request){
+    try {
+        const {userId} = getAuth(request)
+        const isAdmin = await authAdmin(userId)
+
+        if(!isAdmin){
+            return NextResponse.json({ error: "not authorized"} , { status: 401 })
+        }
+
+        const coupons = await prisma.coupon.findMany({}) 
+
+        return NextResponse.json({coupons})
+
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json({ error: error.code || error.message} , {status: 400})        
+    }
+}
