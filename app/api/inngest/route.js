67c8b8b689fdbdf6e926e9fbbece5299f7ff5b8b
Vindya Kodithuwakku <164727679+vindyakodithuwakku02@users.noreply.github.com>
2025-09-22 09:10:26 +0530

    update inngest routes

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,6 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
-import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/inngest/functions";
+import { deleteCoupononExpiry, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/inngest/functions";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
@@ -8,6 +8,7 @@ export const { GET, POST, PUT } = serve({
   functions: [
     syncUserCreation,
     syncUserUpdation,
-    syncUserDeletion
+    syncUserDeletion,
+    deleteCoupononExpiry
   ],
 });
