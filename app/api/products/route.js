c6d49a5b3a75655ad93810e21bf0b33693055509
Vindya Kodithuwakku <164727679+vindyakodithuwakku02@users.noreply.github.com>
2025-09-28 23:19:49 +0530

    update products route

---
+++ app/api/products/route.js
@@ -0,0 +1,31 @@
+const { default: prisma } = require("@/lib/prisma")
+const { NextResponse } = require("next/server")
+
+
+export async function GET(request) {
+    try {
+        let products = await prisma.product.findMany({
+            where: { inStock: true },
+            include: {
+                rating : {
+                    select: {
+                        createdAt: true, rating: true, review: true, user: {
+                            select: {
+                                name: true, image: true
+                            }
+                        }
+                }
+            },
+            store: true,
+        },
+        orderBy: { createdAt: 'desc' }
+        })
+        
+        //remove products with store isActive false
+        products = products.filter(product => product.store.isActive)
+        return NextResponse.json({products})
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 })
+    }
+}
