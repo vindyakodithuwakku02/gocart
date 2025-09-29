f5e55a81fe2530e2e9c6bac7699994d35b59ec20
Vindya Kodithuwakku <164727679+vindyakodithuwakku02@users.noreply.github.com>
2025-09-29 22:41:36 +0530

    update adress routes

--- app/api/address/route.js
+++ app/api/address/route.js
@@ -1,1 +1,22 @@
+import prisma from "@/lib/prisma"
+import { getAuth } from "@clerk/nextjs/server"
+import { NextResponse } from "next/server"
+
+// Add new Address
+export async function POST(request) {
+    try {
+        const { userId } = getAuth(request)
+        const { address } = await request.json()
+
+        address.userId = userId
+
+        const newAddress = await prisma.address.create({
+            data: address
+        })
+        return NextResponse.json({ newAddress, message: 'Address added successfully' })
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json({ error: error.code || error.message }, { status: 400 })
+    }
+}
 
