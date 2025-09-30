d040e2783b5956c8c12d99c3c991c0929d25d877
Vindya Kodithuwakku <164727679+vindyakodithuwakku02@users.noreply.github.com>
2025-09-30 23:17:53 +0530

    update cart routes

--- app/api/cart/route.js
+++ app/api/cart/route.js
@@ -1,39 +1,39 @@
-import { getAuth } from "@clerk/nextjs/dist/types/server";
-import { NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
+// import { getAuth } from "@clerk/nextjs/dist/types/server";
+// import { NextResponse } from "next/server";
+// import prisma from "@/lib/prisma";
 
-//Update user cart
-export async function POST(request) {
-    try {
-        const { userId } = getAuth(request) 
-        const { cart } = await request.json()   
+// //Update user cart
+// export async function POST(request) {
+//     try {
+//         const { userId } = getAuth(request) 
+//         const { cart } = await request.json()   
 
-//save the cart to the user object
-        await prisma.user.update({
-            where: { id: userId },
-            data: { cart: cart }
-        })
-        return NextResponse.json({ message: "Cart updated" })
-    } catch (error) {
-        console.error(error)
-            return NextResponse.json({ error: "Internal Server Error" }, { status: 500 })
-        }
-    }
+// //save the cart to the user object
+//         await prisma.user.update({
+//             where: { id: userId },
+//             data: { cart: cart }
+//         })
+//         return NextResponse.json({ message: "Cart updated" })
+//     } catch (error) {
+//         console.error(error)
+//             return NextResponse.json({ error: "Internal Server Error" }, { status: 500 })
+//         }
+//     }
 
-    // Get user cart
-export async function GET(request) {
-    try {
-        const { userId } = getAuth(request)
+//     // Get user cart
+// export async function GET(request) {
+//     try {
+//         const { userId } = getAuth(request)
 
-        const user = await prisma.user.findUnique({
-            where: { id: userId }
-        })
+//         const user = await prisma.user.findUnique({
+//             where: { id: userId }
+//         })
 
-            return NextResponse.json({ cart: user.cart })
-    } catch (error) {
-        console.error(error)
-        return NextResponse.json({ error: error.message }, { status:400 })
-    }
-}
+//             return NextResponse.json({ cart: user.cart })
+//     } catch (error) {
+//         console.error(error)
+//         return NextResponse.json({ error: error.message }, { status:400 })
+//     }
+// }
 
        
