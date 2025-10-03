641e09b1d2bc83df72afbebbefd123059a1fed74
Vindya Kodithuwakku <164727679+vindyakodithuwakku02@users.noreply.github.com>
2025-10-03 23:10:10 +0530

    update public page

--- app/(public)/page.jsx
+++ app/(public)/page.jsx
@@ -3,7 +3,7 @@ import BestSelling from "@/components/BestSelling";
 import Hero from "@/components/Hero";
 import Newsletter from "@/components/Newsletter";
 import OurSpecs from "@/components/OurSpec";
-// import LatestProducts from "@/components/LatestProducts";
+import LatestProducts from "@/components/LatestProducts";
 
 export default function Home() {
     return (
