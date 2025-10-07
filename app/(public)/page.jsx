23d327700bea20178b3998080c1d066b4c069090
Vindya Kodithuwakku <164727679+vindyakodithuwakku02@users.noreply.github.com>
2025-10-07 23:23:28 +0530

    update public page

--- app/(public)/page.jsx
+++ app/(public)/page.jsx
@@ -10,9 +10,9 @@ export default function Home() {
         <div>
             <Hero />
             <LatestProducts />
-            {/* <BestSelling />
+            <BestSelling />
             <OurSpecs />
-            <Newsletter /> */}
+            <Newsletter />
         </div>
     );
 }
