ff095b29b518dd266df9ee76d18927dd6fefb8d8
Vindya Kodithuwakku <164727679+vindyakodithuwakku02@users.noreply.github.com>
2025-10-15 19:53:01 +0530

    update layout

--- app/(public)/layout.jsx
+++ app/(public)/layout.jsx
@@ -5,14 +5,15 @@ import Footer from "@/components/Footer";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { fetchProducts } from "@/lib/features/product/productSlice";
-import useUser from "@clerk/nextjs";
+import {useUser} from "@clerk/nextjs";
+import { useAuth } from "@clerk/nextjs";
 
 export default function PublicLayout({ children }) {
 
     const dispatch = useDispatch()
     
     const {user} = useUser()
-    
+    const {getToken} = useAuth()
 
 
     useEffect(() => {
