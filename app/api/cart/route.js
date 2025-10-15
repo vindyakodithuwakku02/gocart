8ef13e9b64d845c4f97cbfeab73d3be3a721636e
Vindya Kodithuwakku <164727679+vindyakodithuwakku02@users.noreply.github.com>
2025-10-15 20:09:25 +0530

    update routes

--- app/api/cart/route.js
+++ app/api/cart/route.js
@@ -1,48 +1,38 @@
-import { auth } from "@clerk/nextjs/server";
+import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
 //Update user cart
 export async function POST(request) {
     try {
-        const { userId } = await auth();
-        
-        if (!userId) {
-            return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-        }
-        
-        const { cart } = await request.json();
+        const { userId } = getAuth(request) 
+        const { cart } = await request.json()   
 
-        //save the cart to the user object
+//save the cart to the user object
         await prisma.user.update({
             where: { id: userId },
             data: { cart: cart }
-        });
-        
-        return NextResponse.json({ message: "Cart updated" });
+        })
+        return NextResponse.json({ message: "Cart updated" })
     } catch (error) {
-        console.error(error);
-        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+        console.error(error)
+            return NextResponse.json({ error: "Internal Server Error" }, { status: 500 })
+        }
     }
-}
 
 // Get user cart
 export async function GET(request) {
     try {
-        const { userId } = await auth();
-        
-        if (!userId) {
-            return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-        }
+        const { userId } = getAuth(request)
 
         const user = await prisma.user.findUnique({
             where: { id: userId }
-        });
+        })
 
-        return NextResponse.json({ cart: user.cart });
+            return NextResponse.json({ cart: user.cart })
     } catch (error) {
-        console.error(error);
-        return NextResponse.json({ error: error.message }, { status: 400 });
+        console.error(error)
+        return NextResponse.json({ error: error.message }, { status:400 })
     }
 }
 
