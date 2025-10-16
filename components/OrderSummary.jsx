250ff3e86df868d155c25469b261bc495339c6d0
Vindya Kodithuwakku <164727679+vindyakodithuwakku02@users.noreply.github.com>
2025-10-16 21:57:01 +0530

    update ordersummary

--- components/OrderSummary.jsx
+++ components/OrderSummary.jsx
@@ -163,10 +163,10 @@ const OrderSummary = ({ totalPrice, items }) => {
       <div className="flex justify-between py-4">
         <p>Total:</p>
         <p className="font-medium text-right">
-          {currency}
-          {coupon
-            ? (totalPrice - (coupon.discount / 100 * totalPrice)).toFixed(2)
-            : totalPrice.toLocaleString()}
+          <Protect plan={'plus'} fallback={`${currency}${coupon? (totalPrice + 5 - (coupon.discount / 100 * totalPrice)).toFixed(2)
+            : (totalPrice + 5).toLocaleString()}`}>
+          </Protect>
+          
         </p>
       </div>
 
