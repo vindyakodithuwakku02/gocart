500b196f864e7cab4ada05eb3d7b387b6280f632
Vindya Kodithuwakku <164727679+vindyakodithuwakku02@users.noreply.github.com>
2025-10-16 22:01:36 +0530

    update pricing page

--- app/(public)/pricing/page.jsx
+++ app/(public)/pricing/page.jsx
@@ -4,7 +4,7 @@ export default function PricingPage() {
     return (
         <div className='mx-auto max-w-[700px] my-28'>
             {/* Pricing Table */}
-
+            <PricingTable />
         </div>
     )
 }
