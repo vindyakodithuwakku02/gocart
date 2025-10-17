8f7baf8ed8d5afaedf57face8731c0c9fef6dd4a
Vindya Kodithuwakku <164727679+vindyakodithuwakku02@users.noreply.github.com>
2025-10-17 22:30:47 +0530

    update navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -30,9 +30,9 @@ const Navbar = () => {
           <Link href="/" className="relative text-4xl font-semibold text-slate-700">
             <span className="text-green-600">go</span>cart
             <span className="text-green-600 text-5xl leading-0">.</span>
-            <Protect plan='plus'>
+            {/* <Protect plan='plus'>
               <p className="absolute text-xs font-semibold -top-1 -right-8 px-3 p-0.5 rounded-full flex items-center gap-2 text-white bg-green-500">plus</p>
-            </Protect>
+            </Protect> */}
             
           </Link>
 
