018935db0daf7a0d3977949179dff22d8e84bf8d
Vindya Kodithuwakku <vindyasubodhi@gmail.com>
2025-10-24 20:56:36 +0530

    update coupon routes

---
+++ app/api/coupon/route.js
@@ -0,0 +1,38 @@
+import { useAuth } from "@clerk/nextjs";
+
+
+//Verify coupon 
+export async function POST(request) {
+    try {
+        const { userId, has } = useAuth(request);
+        const { code } = await request.json(); 
+        
+        const coupon = await prisma.coupon.findUnique({ where: { code : code.toUpperCase(),
+            expiresAt : { gt: new Date() }  
+         }
+         })
+
+        if (!coupon) {
+            return NextResponse.json({ error: "Invalid or expired coupon" }, { status: 404 });
+        }
+
+        if(coupon.forNewUser){
+            const userOrders = await prisma.order.findMany({ where: { userId: userId } })
+            if(userOrders.length > 0){
+                return NextResponse.json({ error: "Coupon valid only for new users" }, { status: 400 });
+            }
+        }
+
+        if(coupon.forMember){
+            const hasPlusPlan = has({ plan: "plus" })
+            if(!hasPlusPlan) {
+                return NextResponse.json({ error: "Coupon valid only for members" }, { status: 400 });
+            }
+        }
+
+        return NextResponse.json({ coupon });
+    } catch (error) {
+        console.error( error);
+        return NextResponse.json({ error: error.code || error.message  }, { status: 400 });
+    }
+}
